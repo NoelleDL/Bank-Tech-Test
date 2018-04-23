1f5384164597de6e1cd7b2ed9ecab4fdc577d6d2
NoelleDL <noelle.legrain@gmail.com>
2018-04-23 17:18:52 +0100

    Added test and deposit method

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,7 +3,6 @@ require 'bank_account.rb'
 describe Bank_Account do
 
   let(:subject) { described_class.new }
-  let(:dummy_deposit) { double :dummy_deposit }
 
   describe '#deposit' do
     it 'allows user to deposit money into account' do
